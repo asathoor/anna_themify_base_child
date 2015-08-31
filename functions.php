c4fafe396b94b6c7f544fe0928d4ec471b6b6885
asathoor <per@multimusen.dk>
2015-09-01 00:25:46 +0200

    first upload

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+/* add stylesheets */
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'Mother Stylesheet', get_template_directory_uri() . '/style.css' ); 
+} 
+?>
